42be175236ebeb721ffcce2c55e05f55e4105581
wiwoowuwii <94783226+WiWooWuWii@users.noreply.github.com>
2024-11-28 14:54:11 +0300

    Delete Subtask.java

--- Subtask.java
+++
@@ -1,6 +0,0 @@
-
-public class Subtask extends Task{
-    public Subtask(String number, String status, String title, String description, String priority, String dueDate) {
-        super(number, status, title, description, priority, dueDate);
-    }
-}
